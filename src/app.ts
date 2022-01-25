7ecda9b2681d8ddc7640c28e14bb271286ae5115
oansky <oana.anghelachi@gmail.com>
2022-01-25 14:11:54 +0100

    Select the DOM elements and type cast

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
 // Code goes here!
+
+class Projectnput {
+	templateEl: HTMLTemplateElement
+	hostEl: HTMLDivElement
+
+	constructor() {
+		this.templateEl = document.getElementById(
+			"project-input"
+		)! as HTMLTemplateElement
+		this.hostEl = document.getElementById("app")! as HTMLDivElement
+	}
+}
