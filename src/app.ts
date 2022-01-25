836c4dbf39fae01e3c07fab1629869a9279420c9
oansky <oana.anghelachi@gmail.com>
2022-01-25 14:48:32 +0100

    Interact with DOM elements

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@
 class ProjectInput {
 	templateEl: HTMLTemplateElement
 	hostEl: HTMLDivElement
-    element: HTMLFormElement
+	element: HTMLFormElement
+	titleInputEl: HTMLInputElement
+	descriptionInputEl: HTMLInputElement
+	peopleInputEl: HTMLInputElement
 
 	constructor() {
 		this.templateEl = document.getElementById(
@@ -12,13 +15,29 @@ class ProjectInput {
 		this.hostEl = document.getElementById("app")! as HTMLDivElement
 
 		const importedNode = document.importNode(this.templateEl.content, true)
-        this.element = importedNode.firstElementChild as HTMLFormElement
-        this.attach()
+		this.element = importedNode.firstElementChild as HTMLFormElement
+		this.element.id = "user-input"
+		this.titleInputEl = this.element.querySelector("#title") as HTMLInputElement
+		this.descriptionInputEl = this.element.querySelector(
+			"#description"
+		) as HTMLInputElement
+		this.peopleInputEl = this.element.querySelector(
+			"#people"
+		) as HTMLInputElement
+
+		this.configure()
+		this.attach()
 	}
 
-    private attach() { 
-        this.hostEl.insertAdjacentElement('afterbegin', this.element)
-    }
+	private submitHandler(event: Event) {
+		event.preventDefault
+	}
+	private configure() {
+		this.element.addEventListener("submit", this.submitHandler.bind(this))
+	}
+	private attach() {
+		this.hostEl.insertAdjacentElement("afterbegin", this.element)
+	}
 }
 
 const projectInput = new ProjectInput()
