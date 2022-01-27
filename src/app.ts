e51da71c87b23318ff5eb3d41f59fd278c0cd4f1
oansky <oana.anghelachi@gmail.com>
2022-01-27 01:06:48 +0100

    Fix error for unsued/never read values

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 // method() decorator
-function autobind(target: any, methodName: string, descriptor: PropertyDescriptor) { 
+function autobind( 
+	_: any, 
+	_2: string, 
+	descriptor: PropertyDescriptor 
+) { 
 	const originalMethod = descriptor.value; 
 	const adjDescriptor: PropertyDescriptor = { 
-		configurable: true 
+		configurable: true, 
 		get() { 
 			const boundFn = originalMethod.bind(this)
 			return boundFn
